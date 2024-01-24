dfbd90bf32965a5c921444e334b48a7401cdaa2f
alice.yin <alice.yin@temporal.io>
2024-01-23 22:58:42 -0800

    better wording

--- modules/gcp-sink-sa/main.tf
+++ modules/gcp-sink-sa/main.tf
@@ -4,7 +4,7 @@ locals {
   is_auditlog = length(regexall("auditlog-.*", local.sa_email)) > 0
 }
 
-resource "google_service_account" "test-customer-sa" {
+resource "google_service_account" "gcp_sink_sa" {
   account_id   = var.service_account_id
   display_name = "Service account allow Temporal write to sink"
 }
@@ -12,20 +12,20 @@ resource "google_service_account" "test-customer-sa" {
 resource "google_storage_bucket_iam_member" "service_account_storage_access" {
   role   = "roles/storage.objectCreator"
   bucket = var.sink_name
-  member = "serviceAccount:${google_service_account.test-customer-sa.email}"
+  member = "serviceAccount:${google_service_account.gcp_sink_sa.email}"
   count  = local.is_export ? 1 : 0
 }
 
 resource "google_pubsub_topic_iam_member" "service_account_pubsub_access" {
   role   = "roles/pubsub.publisher"
   topic  = var.sink_name
-  member = "serviceAccount:${google_service_account.test-customer-sa.email}"
+  member = "serviceAccount:${google_service_account.gcp_sink_sa.email}"
   count  = local.is_auditlog ? 1 : 0
 }
 
 resource "google_service_account_iam_member" "service_account_token_creator_role" {
   for_each           = var.temporal_service_account_emails
-  service_account_id = google_service_account.test-customer-sa.id
+  service_account_id = google_service_account.gcp_sink_sa.id
   role               = "roles/iam.serviceAccountTokenCreator"
   member             = "serviceAccount:${each.key}"
 }

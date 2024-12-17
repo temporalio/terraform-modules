7bd5c4388192be0ded6957ee4e9d99dc37ceae9f
Zhengbo Zhou <99763465+jumbogo@users.noreply.github.com>
2024-12-17 14:41:42 -0800

    add buckets get role for region validation (#7)
    * add buckets get role for region validation
    
    * address comment

--- modules/gcp-sink-sa/main.tf
+++ modules/gcp-sink-sa/main.tf
@@ -1,7 +1,7 @@
 locals {
   sa_email    = tolist(var.temporal_service_account_emails)[0]
   is_export   = length(regexall("export-.*", local.sa_email)) > 0
-  is_auditlog = length(regexall("auditlog-.*", local.sa_email)) > 0
+
 }
 
 resource "google_service_account" "gcp_sink_sa" {
@@ -17,11 +17,17 @@ resource "google_storage_bucket_iam_member" "service_account_storage_access" {
   count  = local.is_export ? 1 : 0
 }
 
+resource "google_storage_bucket_iam_member" "service_account_bucket_access" {
+  role   = "roles/storage.buckets.get"
+  bucket = var.destination_name
+  member = "serviceAccount:${google_service_account.gcp_sink_sa.email}"
+  count  = local.is_export ? 1 : 0
+}
+
 resource "google_pubsub_topic_iam_member" "service_account_pubsub_access" {
   role   = "roles/pubsub.publisher"
   topic  = var.destination_name
   member = "serviceAccount:${google_service_account.gcp_sink_sa.email}"
-  count  = local.is_auditlog ? 1 : 0
 }
 
 resource "google_service_account_iam_member" "service_account_token_creator_role" {
@@ -30,4 +36,5 @@ resource "google_service_account_iam_member" "service_account_token_creator_role
   role               = "roles/iam.serviceAccountTokenCreator"
   member             = "serviceAccount:${each.key}"
 }
+
 
